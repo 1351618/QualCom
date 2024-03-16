b4d511a9ad3195470479f5a6b0f2c6517c139493
1351618 <1351618@gmail.com>
2024-03-16 23:35:55 +0200

    add nav header

--- src/shared/ui/PageWrapper/PageWrapper.tsx
+++ src/shared/ui/PageWrapper/PageWrapper.tsx
@@ -1,6 +1,7 @@
-import cls from './pageWrapper.module.scss';
-import { ReactNode } from 'react';
-import { Header } from '../../../widgets/header/ui/Header';
+import cls from "./pageWrapper.module.scss";
+import { ReactNode } from "react";
+import { Header } from "../../../widgets/header/ui/Header";
+import { Footer } from "../../../widgets/footer/ui/Footer";
 
 export const PageWrapper = ({ children }: { children: ReactNode }) => {
   return (
@@ -8,10 +9,12 @@ export const PageWrapper = ({ children }: { children: ReactNode }) => {
       <header className={cls.header__inner}>
         <Header />
       </header>
-      <main className='main'>
-        <div className='Home__inner'>{children}</div>
+      <main className="main">
+        <div className="Home__inner">{children}</div>
       </main>
-      <footer>footer</footer>
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 };
--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,19 +1,16 @@
-import { useTranslation } from 'react-i18next';
-import { useTheme } from '../../../app/providers/themeProvider/lib/useTheme';
-import cls from './header.module.scss';
-import { useState } from 'react';
+import { useTranslation } from "react-i18next";
+import { useTheme } from "../../../app/providers/themeProvider/lib/useTheme";
+import cls from "./header.module.scss";
+import { useState } from "react";
+import { NavHeader } from "../../navHeader/ui/NavHeader";
 
 export const Header = () => {
   const { t, i18n } = useTranslation();
-  const languagesArray = ['ru', 'en', 'cn'];
-
+  const languagesArray = ["ru", "en", "cn"];
   const [counter, setCounter] = useState(0);
-
   const { changeTheme } = useTheme();
 
-  if (counter === languagesArray.length) {
-    setCounter(0);
-  }
+  counter === languagesArray.length ? setCounter(0) : null;
 
   const toggle = async (): Promise<void> => {
     void (await i18n.changeLanguage(languagesArray[counter]));
@@ -21,19 +18,22 @@ export const Header = () => {
 
   return (
     <header className={cls.header}>
-     <div className={cls.text}>
-       {t('Header')}
+      <div className={cls.headerLogo}>
+        <a href="/">Logo</a>
       </div>
-      <button
-        onClick={() => {
-          toggle();
-          setCounter((prev) => prev + 1);
-        }}
-      >
-        {t('Change language mather fucker')}
-      </button>
+      <NavHeader />
+      <div className={cls.headerBacLang}>
+        <button
+          onClick={() => {
+            toggle();
+            setCounter((prev) => prev + 1);
+          }}
+        >
+          {t("Change language")}
+        </button>
 
-      <button onClick={changeTheme}>{t('Change theme')}</button>
+        <button onClick={changeTheme}>{t("Change theme")}</button>
+      </div>
     </header>
   );
 };
