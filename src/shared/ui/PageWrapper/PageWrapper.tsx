87c6737ccab3397481bd27815233357eb9383b3a
1351618 <1351618@gmail.com>
2024-03-16 12:12:29 +0200

    fix 2

--- src/shared/ui/PageWrapper/PageWrapper.tsx
+++ src/shared/ui/PageWrapper/PageWrapper.tsx
@@ -1,19 +1,14 @@
 import cls from "./pageWrapper.module.scss";
 import { ReactNode } from "react";
 import clsx from "clsx";
-// import { useTheme } from "../../../app/providers/themeProvider/lib/useTheme";
 import { Header } from "../../../components/header/header";
 
 export const PageWrapper = ({ children }: { children: ReactNode }) => {
-  // const { changeTheme } = useTheme();
-
   return (
     <div className={clsx(cls.cls, cls.app)}>
-      {/* <header className={cls.header__inner}> */}
-      <Header />
-      {/* <button onClick={changeTheme}>Change theme</button> */}
-      {/* <div className="header__inner"></div> */}
-      {/* </header> */}
+      <header className={cls.header__inner}>
+        <Header />
+      </header>
       <main className="main">
         <div className="Home__inner">{children}</div>
       </main>
