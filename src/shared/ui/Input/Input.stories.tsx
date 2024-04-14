1c15cf4bd1277faef4bfd54f6ddd0521584a7a84
Ismail <hummetzadeismail@gmail.com>
2024-04-14 16:46:06 +0300

    fear: add custom input, в дальнейшем input точно будет еще кастимизирован

---
+++ src/shared/ui/Input/Input.stories.tsx
@@ -0,0 +1,12 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Input } from "./Input";
+
+const meta: Meta<typeof Input> = {
+  title: "shared/ui/Input",
+  component: Input,
+};
+type Story = StoryObj<typeof Input>;
+export const Normal: Story = {
+  args: {},
+};
+export default meta;
